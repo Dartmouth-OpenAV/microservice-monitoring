0b8114d70619e9446e60633c5f1e3e16414c57a8
Jeffrey Cheng <jeffrey.k.cheng@dartmouth.edu>
2025-08-25 16:33:43 -0400

    Initial commit

---
+++ source/api.php
@@ -0,0 +1,61 @@
+<?php
+header('Content-Type: application/json');
+define("NAMESERVER", "129.170.17.4");
+define("EXTERNAL_HOST", "google.com");
+
+$request_method = $_SERVER['REQUEST_METHOD'];
+
+///////////////////////////////////////////////////
+//
+//  Endpoints:
+//  external - ping EXTERNAL_HOST - usage: /external
+//  router - ping router_host - usage: /router/{router_host}
+//  dns - ping NAMESERVER - usage: /dns
+//
+///////////////////////////////////////////////////
+
+$router_host = "";
+$parts = explode('/', $_GET['command'], 2);
+$endpoint = $parts[0];
+if (isset($parts[1])) {
+    $router_host = $parts[1];
+}
+$nameserver = "129.170.17.4";
+$external_host = "google.com";
+$output = '"false"';
+
+if ($request_method == "GET") {
+    if ($endpoint === 'external') {
+        if (get_ping($external_host)) {
+            $output = '"true"';
+        }
+    } elseif ($endpoint === 'router') {
+        if (get_ping($router_host)) {
+            $output = '"true"';
+        }
+    } elseif ($endpoint === 'dns') {
+        if (get_ping($nameserver)) {
+            $output = '"true"';
+        }
+    }
+}
+
+print $output;
+
+
+///////////////////////////////////////////////////
+//
+//  Functions
+//  get_ping($host, $timeout)
+//
+///////////////////////////////////////////////////
+
+function get_ping($host, $timeout = 1) {
+    $command = "ping -c 1 -W " . $timeout . " " . escapeshellarg($host) . " 2>&1";
+
+    $output = [];
+    $result = 0;
+    exec($command, $output, $result);
+
+    return $result === 0;
+}
